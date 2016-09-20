e83f2b465b2ce37e32ea5d45857ecc959dac9b9d
ritzroczka <ritzroczka@protonmail.ch>
2016-09-20 16:18:57 +0700

    clean up & patch applied

---
+++ README.md
@@ -0,0 +1,8 @@
+### rxvt-unicode-patched
+
+patch applied
+
+** font-width-fix.patch
+** line-spacing-fix.patch
+** sgr-mouse-mode.patch
+** enable-wide-glyphs.patch
---
+++ configureit.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# configure script helper
+# randalltux
+
+sh configure \
+  --prefix=/usr \
+  --enable-256-color \
+  --enable-fading \
+  --enable-font-styles \
+  --enable-iso14755 \
+  --enable-keepscrolling \
+  --enable-lastlog \
+  --enable-mousewheel \
+  --enable-next-scroll \
+  --enable-perl \
+  --enable-pointer-blank \
+  --enable-rxvt-scroll \
+  --enable-selectionscrolling \
+  --enable-slipwheeling \
+  --disable-smart-resize \
+  --enable-startup-notification \
+  --enable-transparency \
+  --enable-unicode3 \
+  --enable-utmp \
+  --enable-wide-glyphs \
+  --enable-wtmp \
+  --enable-xft \
+  --enable-xim \
+  --enable-xterm-scroll \
+  --disable-frills
